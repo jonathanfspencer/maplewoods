717020d67bacbd9aaea33731cc3ee3eed3c4cdec
Jonathan Spencer <jonathanfspencer@gmail.com>
2021-03-02 14:33:16 -0600

    structuring

--- treetypes.py
+++ treetypes.py
@@ -16,7 +16,7 @@ pandas.set_option('display.max_rows', None)
 # bug fix for display formats to avoid run time errors
 pandas.set_option('display.float_format', lambda x:'%f'%x)
 
-data = pandas.read_csv('sites.csv', low_memory=False)
+data = pandas.read_csv('data/sites.csv', low_memory=False)
 data = data.replace(r'^\s*$', numpy.NaN, regex=True)
 
 # print out information from the survey
@@ -30,4 +30,4 @@ seaborn.histplot(x="Species", data=data)
 plt.xlabel('Tree Species')
 plt.ylabel('Number of Individuals')
 plt.title('Maplewood Tree Survey Species Breakdown')
-plt.savefig('species.png')
+plt.savefig('images/species.png')
